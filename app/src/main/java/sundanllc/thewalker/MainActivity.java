1028ef348d41c2eca5140d23f247311bf584b70b
Sunnara <sunnyisara@gmail.com>
2017-02-06 16:05:45 -0500

    Created AboutPage, CreateMenu, and the connected XML Layouts

--- app/src/main/java/sundanllc/thewalker/MainActivity.java
+++ app/src/main/java/sundanllc/thewalker/MainActivity.java
@@ -1,13 +1,41 @@
 package sundanllc.thewalker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loadButton, createButton, aboutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadButton = (Button) findViewById(R.id.load_button);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateMenu.class)); //change to PlayMenu.class once created
+            }
+        });
+
+        createButton = (Button) findViewById(R.id.create_button);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateMenu.class));
+            }
+        });
+
+        aboutButton = (Button) findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CreateMenu.class));
+            }
+        });
     }
 }
