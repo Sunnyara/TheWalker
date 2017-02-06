a4f461304d83323a9f0a690174b7fa0ca493d3c8
dcnance <nancedc@appstate.edu>
2017-02-06 17:43:41 -0500

    Added share and search drawables. Changed buttons on Play from text to image buttons

--- app/src/main/java/sundanllc/thewalker/CreateMenu.java
+++ app/src/main/java/sundanllc/thewalker/CreateMenu.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CreateMenu extends AppCompatActivity {
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
