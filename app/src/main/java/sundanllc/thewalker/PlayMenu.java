1b9a680d5c9bfcd72d5bacb5a566f7db6c557f61
dcnance <nancedc@appstate.edu>
2017-02-06 17:14:14 -0500

    Added "Share" drawable and created menu in Play screen

--- app/src/main/java/sundanllc/thewalker/PlayMenu.java
+++ app/src/main/java/sundanllc/thewalker/PlayMenu.java
@@ -1,8 +1,17 @@
 package sundanllc.thewalker;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Daniel on 2/6/2017.
  */
 
-public class PlayMenu {
+public class PlayMenu extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity);
+    }
 }
