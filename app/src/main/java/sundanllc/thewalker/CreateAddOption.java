e56b69e4f80fc935ada47b140ad0db11a588d94d
Sunnara <sunnyisara@gmail.com>
2017-02-08 15:49:33 -0500

    String inputs

---
+++ app/src/main/java/sundanllc/thewalker/CreateAddOption.java
@@ -0,0 +1,17 @@
+package sundanllc.thewalker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sunnara on 2/8/2017.
+ */
+
+public class CreateAddOption extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.create_screen_add);
+    }
+}
