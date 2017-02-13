dfa4845ce6898a6fd602f77892ce036c683d2df8
Sunnara <sunnyisara@gmail.com>
2017-02-13 15:45:34 -0500

    Updates

---
+++ app/src/main/java/sundanllc/thewalker/Checkpoint.java
@@ -0,0 +1,38 @@
+package sundanllc.thewalker;
+
+/**
+ * Created by Sunnara on 2/8/2017.
+ */
+
+public class Checkpoint {
+    private String mAddress;
+    private String mX;
+    private String mY;
+
+
+    public String getmX() {
+        return mX;
+    }
+
+    public void setmX(String mX) {
+        this.mX = mX;
+    }
+
+    public String getmAddress() {
+        return mAddress;
+    }
+
+    public void setmAddress(String mAddress) {
+        this.mAddress = mAddress;
+    }
+
+    public String getmY() {
+        return mY;
+    }
+
+    public void setmY(String mY) {
+        this.mY = mY;
+    }
+
+
+}
--- app/src/main/java/sundanllc/thewalker/CreateAddOption.java
+++ app/src/main/java/sundanllc/thewalker/CreateAddOption.java
@@ -2,6 +2,15 @@ package sundanllc.thewalker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Sunnara on 2/8/2017.
@@ -9,9 +18,53 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CreateAddOption extends AppCompatActivity{
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapater;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button addCP, removeCP;
+    private TextView addString, xString, yString;
+
     @Override
     public void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.create_screen_add);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.checkpoint_create);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        addCP = (Button) findViewById(R.id.create_add_checkpoint);
+        addCP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        removeCP = (Button) findViewById(R.id.create_remove_checkpoint);
+        removeCP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public class CheckpointAdapters extends RecyclerView.Adapter<CheckpointAdapters.ViewHolder> {
+        private String[] mItemList;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView addString, xString, yString;
+
+            public ViewHolder (TextView v) {
+                super(v);
+            }
+
+
+        }
     }
 }
