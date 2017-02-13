e7dd416252212eeb85989f73dfff9c459801f863
dcnance <nancedc@appstate.edu>
2017-02-13 17:24:46 -0500

    MADE A RECYCLERVIEW!

---
+++ app/src/main/java/sundanllc/thewalker/PlayAdapter.java
@@ -0,0 +1,56 @@
+package sundanllc.thewalker;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static sundanllc.thewalker.R.id.titleView;
+
+/**
+ * Created by Daniel on 2/13/2017.
+ */
+
+public class PlayAdapter extends RecyclerView.Adapter<PlayAdapter.ViewHolder> {
+
+    private String[][] dataset;
+
+    public PlayAdapter(String[][] data)
+    {
+        dataset = data;
+    }
+
+    @Override
+    public PlayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.play_list_fragment, parent, false);
+        return new ViewHolder(inflatedView);
+    }
+
+    @Override
+    public void onBindViewHolder(PlayAdapter.ViewHolder holder, int position)
+    {
+        holder.titleView.setText(dataset[position][0]);
+        holder.authorView.setText(dataset[position][1]);
+        holder.etaView.setText(dataset[position][2]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView titleView, authorView, etaView;
+
+        public ViewHolder(View v)
+        {
+            super(v);
+            titleView = (TextView) v.findViewById(R.id.titleView);
+            authorView = (TextView) v.findViewById(R.id.authorView);
+            etaView = (TextView) v.findViewById(R.id.etaView);
+        }
+    }
+}
