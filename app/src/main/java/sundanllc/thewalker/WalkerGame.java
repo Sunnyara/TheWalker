b1470a907cb51760297b2ecccc4d686e49cea491
Sunnara <sunnyisara@gmail.com>
2017-02-27 16:07:16 -0500

    Created Walkergame class

---
+++ app/src/main/java/sundanllc/thewalker/WalkerGame.java
@@ -0,0 +1,77 @@
+package sundanllc.thewalker;
+
+/**
+ * Created by Sunnara on 2/27/2017.
+ */
+
+public class WalkerGame {
+    private int id;
+    private String title, author, description, picture;
+    private int eta, time_played;
+
+    public WalkerGame(String title, String author, String description, String picture, int eta,
+                      int time_played) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.picture = picture;
+        this.eta = eta;
+        this.time_played = time_played;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public int getTime_played() {
+        return time_played;
+    }
+
+    public void setTime_played(int time_played) {
+        this.time_played = time_played;
+    }
+}
