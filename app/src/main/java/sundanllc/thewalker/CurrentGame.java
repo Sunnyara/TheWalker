9c81b8b60b5a373b9b69d6977db3a0d1ecde7e84
Sunnara <sunnyisara@gmail.com>
2017-02-27 18:20:03 -0500

    Creating Fragment

---
+++ app/src/main/java/sundanllc/thewalker/CurrentGame.java
@@ -0,0 +1,31 @@
+package sundanllc.thewalker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sunnara on 2/27/2017.
+ */
+
+public class CurrentGame extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
